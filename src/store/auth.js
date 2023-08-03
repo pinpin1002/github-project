75d680003fc5fa8f67c39e36156fcc7058491f73
Ivan <pinpin1002@hotmail.com>
2023-08-03 14:15:47 +0800

    Update | cdn

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,66 +1,68 @@
 import router from "@/router/index.js";
 import $axios from "@/axios";
-import { Octokit } from "https://cdn.skypack.dev/octokit";
+import { Octokit } from "https://esm.sh/@octokit/core";
 
 const auth = {
-  namespaced: true,
-  state() {
-    return {
-      octokit: null,
-      accessToken: null,
-      userInfo: {},
-    };
-  },
-  mutations: {
-    setLogin(state, payload) {
-      state.accessToken = payload;
-    },
-    setUserInfo(state, payload) {
-      state.userInfo = { ...payload };
-    },
-    setOctokit(state) {
-      state.octokit = new Octokit({
-        auth: localStorage.getItem("accessToken"),
-      });
-    },
-    setLogOut(state) {
-      state.accessToken = null;
-      state.userInfo = {};
-      window.localStorage.removeItem("accessToken");
-      router.push("/login");
-    },
-  },
-  actions: {
-    async getLogin({ commit }) {
-      const codeParam = new URLSearchParams(window.location.search).get("code");
-      const {
-        data: { access_token },
-      } = await $axios.get(`http://localhost:4000/getAccessToken?code=${codeParam}`);
+	namespaced: true,
+	state() {
+		return {
+			octokit: null,
+			accessToken: null,
+			userInfo: {},
+		};
+	},
+	mutations: {
+		setLogin(state, payload) {
+			state.accessToken = payload;
+		},
+		setUserInfo(state, payload) {
+			state.userInfo = { ...payload };
+		},
+		setOctokit(state) {
+			state.octokit = new Octokit({
+				auth: localStorage.getItem("accessToken"),
+			});
+		},
+		setLogOut(state) {
+			state.accessToken = null;
+			state.userInfo = {};
+			window.localStorage.removeItem("accessToken");
+			router.push("/login");
+		},
+	},
+	actions: {
+		async getLogin({ commit }) {
+			const codeParam = new URLSearchParams(window.location.search).get("code");
+			const {
+				data: { access_token },
+			} = await $axios.get(
+				`http://localhost:4000/getAccessToken?code=${codeParam}`
+			);
 
-      if (access_token) {
-        localStorage.setItem("accessToken", access_token);
-        commit("setLogin", access_token);
-        router.push("/");
-      }
-    },
-    async getUserInfo({ commit }) {
-      try {
-        const {
-          data: { login, name, avatar_url, public_repos },
-          status,
-        } = await $axios.get(`https://api.github.com/user`, {
-          headers: {
-            Authorization: "token " + localStorage.getItem("accessToken"),
-          },
-        });
-        if (status === 200) {
-          commit("setUserInfo", { login, name, avatar_url, public_repos });
-        }
-      } catch (error) {
-        console.log("error", error);
-      }
-    },
-  },
+			if (access_token) {
+				localStorage.setItem("accessToken", access_token);
+				commit("setLogin", access_token);
+				router.push("/");
+			}
+		},
+		async getUserInfo({ commit }) {
+			try {
+				const {
+					data: { login, name, avatar_url, public_repos },
+					status,
+				} = await $axios.get(`https://api.github.com/user`, {
+					headers: {
+						Authorization: "token " + localStorage.getItem("accessToken"),
+					},
+				});
+				if (status === 200) {
+					commit("setUserInfo", { login, name, avatar_url, public_repos });
+				}
+			} catch (error) {
+				console.log("error", error);
+			}
+		},
+	},
 };
 
 export default auth;
